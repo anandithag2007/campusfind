c301552eddb72bb75fb8dd54b4204800846c15ee
Ananditha G <anandithag2007@gmail.com>
2026-06-28 23:48:37 +0530

    Setup frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div>
+      <h1>CampusFind</h1>
+
+      <h2>Lost & Found Management System</h2>
+
+      <p>
+        Helping students recover lost items quickly and efficiently.
+      </p>
+
+      <hr />
+
+      <h3>Features</h3>
+
+      <ul>
+        <li>Report Lost Items</li>
+        <li>Report Found Items</li>
+        <li>Claim Verification</li>
+        <li>Admin Dashboard</li>
+      </ul>
+
+      <button>Report Lost Item</button>
+      <button>Report Found Item</button>
+    </div>
+  );
+}
+
+export default App;
