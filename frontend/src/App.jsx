31e799c41e36452efbe3adbce6672c5ef7319efa
Ananditha G <anandithag2007@gmail.com>
2026-06-29 23:01:10 +0530

    Add report lost item form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [itemName, setItemName] = useState("");
+  const [location, setLocation] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/lost-items")
@@ -13,6 +15,27 @@ function App() {
   return (
     <div className="container">
       <h1>CampusFind</h1>
+      <h2>Report Lost Item</h2>
+
+<input
+  type="text"
+  placeholder="Item Name"
+  value={itemName}
+  onChange={(e) => setItemName(e.target.value)}
+/>
+
+<br /><br />
+
+<input
+  type="text"
+  placeholder="Location"
+  value={location}
+  onChange={(e) => setLocation(e.target.value)}
+/>
+
+<br /><br />
+
+<button>Report Item</button>
       <h2>Lost Items</h2>
 
       {items.map((item) => (
