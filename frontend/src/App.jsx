88071fd7961bdd40870e8de9eb71084210479745
Ananditha G <anandithag2007@gmail.com>
2026-06-30 09:37:19 +0530

    Add report item form validation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,24 @@ function App() {
       .then((data) => setItems(data));
   }, []);
 
+  const addItem = () => {
+  if (!itemName || !location) {
+    alert("Please fill all fields");
+    return;
+  }
+
+  const newItem = {
+    id: items.length + 1,
+    item: itemName,
+    location: location,
+  };
+
+  setItems([...items, newItem]);
+
+  setItemName("");
+  setLocation("");
+};
+
   return (
     <div className="container">
       <h1>CampusFind</h1>
@@ -35,8 +53,7 @@ function App() {
 
 <br /><br />
 
-<button>Report Item</button>
-      <h2>Lost Items</h2>
+<button onClick={addItem}>Report Item</button>      <h2>Lost Items</h2>
 
       {items.map((item) => (
         <div className="card" key={item.id}>
